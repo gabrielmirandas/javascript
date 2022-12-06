cda431e4dc25d2989180e270d24c9beb789e7417
Gabriel Miranda <118637292+gabrielmirandas@users.noreply.github.com>
2022-12-06 04:51:05 -0300

    exercício js

---
+++ exercicios/aula11/ex7.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade de seu carro é de ${vel}km/h`)
+if(vel > 60){
+    console.log('Você está acima do limite de velocidade. Multado!')
+} 
+console.log(`Dirija sempre com segurança.`)
