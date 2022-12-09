d87e9a5f5f43b8819b027a74b9776b6db2bc3b31
Gabriel Miranda <gdesmiranda07@gmail.com>
2022-12-09 05:18:09 -0300

    exercícios js

---
+++ exercicios/aula15/ex2/modelo/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+var msg= window.document.getElementById('msg');
+var img= window.document.getElementById('imagem');
+var data= new Date();
+var hora= data.getHours();
+var hora= 21
+msg.innerHTML= `Agora são ${hora}hrs`;
+if(hora >= 0 && hora < 12){
+    img.src= '../imagens/manha.jpeg'
+    document.body.style.background = 'blue'; 
+}else if (hora >= 12 && hora < 18) {
+        img.src= '../imagens/tarde.jpeg'
+        document.body.style.background = 'red';
+    }else {
+    img.src= '../imagens/noite.jpeg'
+    document.body.style.background = 'gray';
+ }
+}
